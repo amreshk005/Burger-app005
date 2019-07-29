c345f37496af84018d4e3451443af9a7df2c055d
amreshk005 <amreshk005@gmail.com>
2019-07-30 00:04:49 +0530

    Updates

---
+++ precache-manifest.83fcadbc0cc76b66be3f54137c6905d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896ef1b8bf96589f0c85dec218efbe82",
+    "url": "/amreshk005/burger-app.git/index.html"
+  },
+  {
+    "revision": "d2ef498a112156997b74",
+    "url": "/amreshk005/burger-app.git/static/css/main.177c70d7.chunk.css"
+  },
+  {
+    "revision": "847d2627404d7dc0b20b",
+    "url": "/amreshk005/burger-app.git/static/js/2.c8c54988.chunk.js"
+  },
+  {
+    "revision": "d2ef498a112156997b74",
+    "url": "/amreshk005/burger-app.git/static/js/main.51d30ee7.chunk.js"
+  },
+  {
+    "revision": "47212d70d02d13811a1c",
+    "url": "/amreshk005/burger-app.git/static/js/runtime~main.7c6b4574.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/amreshk005/burger-app.git/static/media/burger-logo.b8503d26.png"
+  }
+]);
